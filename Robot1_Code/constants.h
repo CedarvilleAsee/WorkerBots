1cfd7bab49476df1daecee0892f91b52f4647285
Joel Beckmeyer <joel@thebeckmeyers.xyz>
2018-04-12 13:57:31 -0400

    adjust servo constants

--- Robot1_Code/constants.h
+++ Robot1_Code/constants.h
@@ -7,7 +7,7 @@ const int ORANGE = 48;
 const int PICK_UP = 85;
 const int WHITE = 115;
 const int BALL_TRIGGER = 5000;
-const int SORT_TIME = 750;
+const int SORT_TIME = 50;
 
 //speed constants
 const int SLOW_SPEED = 70;
@@ -27,8 +27,18 @@ const bool WHEEL_BACKWARDS  = false;
 const bool FORWARDS         = true;
 const bool BACKWARDS        = false;
 
-const int DONT_DUMP_POS = 52;
-const int DUMP_POS      = 142;
+const int R_DONT_DUMP = 100;
+const int L_DONT_DUMP = 32;
+const int R_DO_DUMP = 165;
+const int L_DO_DUMP = 85;
+
+// arm constants
+const int L_ARM_DOWN = 90;
+const int R_ARM_DOWN = 153;
+const int L_ARM_UP = 170;
+const int R_ARM_UP = 80;
+const int L_ARM_HALF = 115;
+const int R_ARM_HALF = 130;
 
 // Turn sequence
 const char TURN_SEQUENCE[] = {
