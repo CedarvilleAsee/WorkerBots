7556c2ecf36ed49b2fd4f7b97c9ce1d51323961b
Daschel Fortner <dascheltfortner@gmail.cedarville.edu>
2018-04-21 10:01:36 -0400

    Add the stuff

--- Robot1_Code/Robot1_Code.ino
+++ Robot1_Code/Robot1_Code.ino
@@ -13,8 +13,6 @@ int amountSeen     = 0;
 int printVar = 0;
 bool otherPrintVar = false; // Consider making "printVar" an array
 
-int iterations = 0;
-
 Servo rightArm;
 Servo leftArm;
 Servo sorter;
@@ -51,15 +49,15 @@ void setup() {
   pinMode(L_DUMP,INPUT);
 
   leftArm.attach(L_TUBE);
-	rightArm.attach(R_TUBE);
+  rightArm.attach(R_TUBE);
 
   sorter.attach(SORTER);
   sorter.write(PICK_UP);
 
-	// arms should be written to x_ARM_UP, and then put down when we actually
-	// start
+  // arms should be written to x_ARM_UP, and then put down when we actually
+  // start
   leftArm.write(L_ARM_UP);
-	rightArm.write(R_ARM_UP);
+  rightArm.write(R_ARM_UP);
 
   leftDump.attach(L_DUMP);
   leftDump.write(L_DONT_DUMP);//initalize servo at perfect position IMH.
@@ -74,6 +72,7 @@ void setup() {
   writeWheelDirection(WHEEL_FORWARDS, WHEEL_FORWARDS);
   digitalWrite(WHEEL_STBY  , HIGH);
 
+  Serial.begin(9600);
   Serial3.begin(115200);
   Serial3.println("Starting Up...");
 
@@ -81,7 +80,7 @@ void setup() {
   //Serial.begin(9600);
   Wire.begin();
   delay(500);
-	// MAKE SURE TO CHECK FOR NO YELLOW LED!
+  // MAKE SURE TO CHECK FOR NO YELLOW LED!
   if(!colorSensor.begin()) {
     digitalWrite(LEDY, HIGH);
   }
@@ -91,20 +90,21 @@ void setup() {
 }
 
 /*
-                            MANUEVERING:
+MANUEVERING:
 
-  The following functions relate to maneuvering the robot around the track, and
-  resulting states for accomplishing that task.
+The following functions relate to maneuvering the robot around the track, and
+resulting states for accomplishing that task.
 
-*/
+ */
 
 /*
-  Reads in the line sensor's current state, and stores them in the global
-  variables above.
-*/
+   Reads in the line sensor's current state, and stores them in the global
+   variables above.
+ */
 void readLine() {
   amountSeen    = 0;
   lastLineIndex = -1;
+  firstLineIndex = -1;
   for(int i = 7; i >= 0; --i) {
     sensors[i] = digitalRead(LINE_SENSOR[i]);
     if(sensors[i] == HIGH) {
@@ -117,13 +117,8 @@ void readLine() {
   }
 }
 
-bool atMiddle() {
-  bool exclusivityBool = true;
-  for(int i = 0; i < 3; ++i) {
-    exclusivityBool &= (sensors[i] == LOW && sensors[7 - i] == LOW);
-  }
-  return (sensors[TARGET_INDEX] == HIGH && sensors[TARGET_INDEX + 1] == HIGH
-          && exclusivityBool);
+bool twoConsecutiveAtMiddle() {
+  return twoConsecutive() && firstLineIndex >= TARGET_INDEX;
 }
 
 bool twoConsecutive() {
@@ -145,10 +140,10 @@ bool twoConsecutive() {
 }
 
 /*
-  New and improved wheel direction switch-er-oo. Use the constants FORWARDS and
-  and BACKWARDS for this function. You could use true and false, but that would
-  be very ape-ish of you.
-*/
+   New and improved wheel direction switch-er-oo. Use the constants FORWARDS and
+   and BACKWARDS for this function. You could use true and false, but that would
+   be very ape-ish of you.
+ */
 void writeWheelDirection(bool ldir, bool rdir) {
   digitalWrite(WHEEL_DIR_LF, ldir);
   digitalWrite(WHEEL_DIR_LB, !ldir);
@@ -175,17 +170,17 @@ void old_writeWheelDirection(bool ldir, bool rdir) {
 }
 
 /*
-  Beautiful absolute value function retreived from stack overflow. Behold it!
-*/
+   Beautiful absolute value function retreived from stack overflow. Behold it!
+ */
 int absVal(int val) {
   return val * ((val > 0) - (val < 0));
 }
 
 /*
-  Write a direction to the wheels. If the wheel speed passed is negative, then
-  the direction of that wheel is switched. Don't pass something over 255 or
-  under 0.
-*/
+   Write a direction to the wheels. If the wheel speed passed is negative, then
+   the direction of that wheel is switched. Don't pass something over 255 or
+   under 0.
+ */
 void writeToWheels(int ls, int rs) {
   writeWheelDirection(ls > 0, rs > 0); // Clean? Maybe more confusing?
   if(rs > 0) {
@@ -203,22 +198,41 @@ void writeToWheels(int ls, int rs) {
 }
 
 /*
-  Follows the line, returns true if the robot is at a fork.
-*/
+   Follows the line, returns true if the robot is at a fork.
+ */
 bool lineFollow(int ts, int strictness) {
   int offset = firstLineIndex - TARGET_INDEX;
   int rightSpeed = ts - offset * strictness;
   int leftSpeed = ts + offset * strictness;
-  writeToWheels(leftSpeed % 255, rightSpeed % 255);
+
+  if(amountSeen == 0) {
+    writeToWheels(HALF_SPEED, HALF_SPEED);
+  } else {
+    writeToWheels(leftSpeed % 255, rightSpeed % 255);
+  }
 
   // Return true if the sensors can see a fork
   return amountSeen > TURN_AMOUNT;
 }
 
+bool funnelState(int ts, int strictness) {
+  if(firstLineIndex == -1 && lastLineIndex == -1) {
+    writeToWheels(HALF_SPEED, HALF_SPEED);
+  } else {
+    if(firstLineIndex <= TARGET_INDEX) {
+      writeToWheels(HALF_SPEED, HALF_SPEED - (strictness * firstLineIndex));
+    } else {
+      writeToWheels(HALF_SPEED - (strictness * (8 - firstLineIndex)), HALF_SPEED);
+    }
+  }
+
+  return amountSeen > TURN_AMOUNT;
+}
+
 /*
-  Turns based on direction. Use the constants LEFT and RIGHT for this function.
-  Returns true if the robot is back on the line.
-*/
+   Turns based on direction. Use the constants LEFT and RIGHT for this function.
+   Returns true if the robot is back on the line.
+ */
 bool turn(int spd, char dir) {
   if(dir == LEFT){
     writeToWheels(-spd, spd);
@@ -231,9 +245,14 @@ bool turn(int spd, char dir) {
   //return firstLineIndex >= targetIndex  && amountSeen < 3;
 }
 
+bool correctJank(int spd, int strictness) {
+  writeToWheels(spd - strictness, spd);
+  return twoConsecutiveAtMiddle();
+}
+
 /*
-  Delays for ms number of milliseconds.
-*/
+   Delays for ms number of milliseconds.
+ */
 bool delayState(int ms) {
   static int milliseconds = -1;
   if(milliseconds == -1) {
@@ -247,14 +266,14 @@ bool delayState(int ms) {
 }
 
 /*
-  Switches the wheels backwards and then does a big, swooping turn.
-*/
+   Switches the wheels backwards and then does a big, swooping turn.
+ */
 bool swoopTurn(char dir, int ts, int d) {
   if(dir == LEFT) {
     //lift up arms.
-    writeToWheels(0, ts);
+    writeToWheels((0 - ts) % 5, ts);
   } else {
-    writeToWheels(ts, 0);
+    writeToWheels(ts, (0 - ts) % 20);
   }
 
   return delayState(d);
@@ -264,18 +283,25 @@ bool backToCornerState(int ts, int strictness)
 {
   //writeToWheels(ts, ts);
   int lSens, rSens;
-
+  int offset;
 
   // Get info from wall sensor and update lSens and rSens
   lSens = readBackLeft();
   rSens = readBackRight();
 
-  /*int offset = (lSens - rSens) / strictness;
-  //offset = 10;*/
-  writeToWheels(-SLOW_SPEED, -SLOW_SPEED);
+  if(lSens < 900 && rSens < 900) {
+    offset = (lSens - rSens) / strictness;
 
+    if(absVal(offset) > 255 - ts) {
+      int sign = offset / absVal(offset);
+      offset = (255 - ts) * sign;
+    }
+  } else {
+    offset = 0;
+  }
 
-  return rSens <= 65; // This is because we don't know what the sensors will return (they aren't always the same)
+  writeToWheels(-(ts + offset), -(ts - offset));
+  return rSens <= 60 && lSens <= 60; // This is because we don't know what the sensors will return (they aren't always the same)
   // FIXME The return needs to be tested
 
   //return delayState(d);
@@ -293,40 +319,71 @@ bool waitState() {
 }
 
 /*
-  Proven to work by cases.
-*/
-bool doTurnSequence(const char sequence[], int index) {
+   Proven to work by cases.
+ */
+bool doTurnSequence(const char sequence[], int index, bool left, bool right) {
   static bool turning = false;
-  sortBalls(turning);
-  otherPrintVar = turning;
   if(turning) {
-    if(turn(100, sequence[index])) {
+    if(left) {
+      leftArm.write(L_ARM_HALF);
+    } 
+
+    if(right) {
+      rightArm.write(R_ARM_HALF);
+    }
+
+    if(turn(HALF_SPEED, sequence[index])) {
       turning = false;
       return true;
     }
   } else {
-    turning = lineFollow(100, 50);
+    turning = lineFollow(HALF_SPEED, 50);
   }
   return false;
 }
 
+bool doTurnSequence(const char sequence[], int index) {
+  static bool turning = false;
+  if(turning) {
+    if(turn(HALF_SPEED, sequence[index])) {
+      turning = false;
+      return true;
+    }
+  } else {
+    turning = lineFollow(HALF_SPEED, 50);
+  }
+  return false;
+}
+
+
+
 /*
-  Inductively proven to work.
-*/
+   Inductively proven to work.
+ */
 bool followTrackState() {
   static int state = 0;
   printVar = state;
   bool isFinished = false;
 
   switch(state) {
-    case 3:
+    case 0:
+    case 1:
+      rightArm.write(R_ARM_HALF);
+      leftArm.write(L_ARM_DOWN);
+      break;
+    case 5:
+      if(doTurnSequence(TURN_SEQUENCE, state, false, true)) {
+        state++;
+      }
+      break;
+    case 6:
       rightArm.write(R_ARM_HALF);
       break;
-    case 9:
-    case 10:
+    case 11:
+    case 12:
       leftArm.write(L_ARM_HALF);
       break;
-    case 14:
+    case 16:
       isFinished = amountSeen > TURN_AMOUNT;
       break;
     default:
@@ -335,26 +392,34 @@ bool followTrackState() {
       break;
   }
 
-  if(doTurnSequence(TURN_SEQUENCE, state))
-    state++;
+  if(state != 5) {
+    if(doTurnSequence(TURN_SEQUENCE, state)) {
+      state++;
+    }
+  }   
+
+  sortBalls();
+
   return isFinished;
 }
 
 /*
-  HEY ISAIAH!
-    This is the big thing you need to work on. Here is where most of the work
-    needs done.
-*/
+   HEY ISAIAH!
+   This is the big thing you need to work on. Here is where most of the work
+   needs done.
+ */
 bool cornerState(char dir) {
   static int state = 0;
   printVar = state;
   switch(state)
   {
     case 0:
-      if(swoopTurn(dir, -255, 2150))  state ++;
+      leftArm.write(L_ARM_HALF);
+      rightArm.write(R_ARM_HALF);
+      if(swoopTurn(dir, -255, 2250))  state ++;
       break;
     case 1:
-      if(backToCornerState(100, 10)) {
+      if(backToCornerState(SLOW_SPEED, 20)) {
         state++;
 
         //writeWheelDirection(WHEEL_FORWARDS, WHEEL_FORWARDS);
@@ -363,11 +428,11 @@ bool cornerState(char dir) {
     case 2:
       writeToWheels(0, 0);
       // Add the drop off
-			if(dir == RIGHT) {
-				leftDump.write(L_DO_DUMP);//added 90 to initial position
-			} else {
-				rightDump.write(R_DO_DUMP);
-			}
+      if(dir == RIGHT) {
+        leftDump.write(L_DO_DUMP);//added 90 to initial position
+      } else {
+        rightDump.write(R_DO_DUMP);
+      }
       if(delayState(2000) && dir == RIGHT) {
         state++;
       }
@@ -377,42 +442,34 @@ bool cornerState(char dir) {
       }
       break;
     case 3:
+      if(turnFromCorner(530)) {
+        state++;
+      }
+      break;
+    case 4:
       /*
-        In this state, we want to swing the robot to the right. It needs to
-        turn to the right so that we can get it to the straight line beside
-        the corner. We exit this state after a couple of milliseconds of delay.
-        Once this state is over, the robot should be pointed towards the line
-        and ready to go onward to the line.
-      */
-      leftDump.write(L_DONT_DUMP);
+         In this state, we want to swing the robot to the right. It needs to
+         turn to the right so that we can get it to the straight line beside
+         the corner. We exit this state after a couple of milliseconds of delay.
+         Once this state is over, the robot should be pointed towards the line
+         and ready to go onward to the line.
+       */
+      if(dir == LEFT) {
+        rightDump.write(R_DONT_DUMP);
+      } else {
+        leftDump.write(L_DONT_DUMP);
+      }
 
-      if(turnFromWall(150)) { //TODO: tweak delay value
+      if(funnelState(HALF_SPEED, 40)) { //TODO: tweak delay value
         state++;
         // state = 0;
         // ^- if you want to test just this state, use state = 0, not state++
       }
       break;
-    case 4: // after turning from the wall, in this state we find the line
-      /*
-        This state takes the robot to the line and stops when you actually find
-        the line.
-      */
-      if(findLine(150)) {
-        state++;
-        // state = 0;
-        // ^- if you want to test just this state, use state = 0, not state++
-      }
-      break;
-    case 5: // this state gets the robot back into line following
-      /*
-        In this state, we need to straighten ourselves out a bit before we 
-        kick into line following in the next state. So, we are trying to do a 
-        turn here to treat it like a fork. I don't know that this will work. 
-        So, you might need to add a new state that does a gradual turn to the 
-        left for a couple of milliseconds to get it in a suitable line follow 
-        state. 
-      */
-      if(turn(50, LEFT)) {
+    case 5: // In this state, we work to make it past the fork so we're ready
+            // to do the turn sequence
+      writeToWheels(HALF_SPEED, HALF_SPEED);
+      if(twoConsecutive()) {
         state = 0;
         return true;
       }
@@ -431,7 +488,7 @@ bool goToNextCornerState() {
   if(doTurnSequence(SECOND_TURN_SEQUENCE, state))
     state++;
 
-  return state == 3 && amountSeen > TURN_AMOUNT;
+  return state == 2 && amountSeen > TURN_AMOUNT;
 }
 
 bool secondCornerState() {
@@ -463,17 +520,17 @@ bool secondCornerState() {
   return false;
 }
 
-bool turnFromWall(int d) {
-	writeToWheels(150,0);
-	return delayState(d);
+bool turnFromCorner(int d) {
+  writeToWheels(150,0);
+  return delayState(d);
 }
 
 /*
-                               COLOR SORTING:
+   COLOR SORTING:
 
-  The following functions all relate to color sorting.
+   The following functions all relate to color sorting.
 
-*/
+ */
 
 bool isBallPresent() {
   return colorSensor.getWhite() > BALL_TRIGGER;
@@ -483,9 +540,9 @@ bool isBallPresent() {
 // If not, move the other way.
 int getPositionFromBall() {
   if(colorSensor.getRed() > colorSensor.getGreen()){
-     return ORANGE;
+    return ORANGE;
   } else {
-        return WHITE;
+    return WHITE;
   }
 }
 
@@ -495,39 +552,44 @@ bool sort(int color) {
 }
 
 // We sort only when a ball is present.
-void sortBalls(bool turning) {
-	static bool sorting = false;
-	static int wiggleWiggleWiggle = 30;
-	static unsigned int currentTime = -1;
-	if(sorting) {
-		sorting = !sort(getPositionFromBall());
-	} else {
+void sortBalls() {
+  static bool sorting = false;
+  static bool leftWiggle = true;
+  static int  wiggleStart = millis();
+  const  int  WIGGLE = 6;
+  if(sorting) {
+    sorting = !sort(getPositionFromBall());
+  } else {
 
-		// this code makes the sorter servo vibrate to get better results
-		if(currentTime == -1) {
-			currentTime = millis();
-		}
-		if( (millis() - currentTime ) > 700) {
-			sorter.write(PICK_UP + wiggleWiggleWiggle);
-			wiggleWiggleWiggle = -wiggleWiggleWiggle;
-			currentTime = -1;
-		} else {
-			sorter.write(PICK_UP);
-		}
-		sorting = isBallPresent(); // sorting = turning;
-	}
+    // If it's time to move left
+    if(leftWiggle) {
+      // move left
+      sorter.write(PICK_UP + WIGGLE);
+    } else {
+      // move right
+      sorter.write(PICK_UP - WIGGLE);
+    }
+
+    // If we're done in one direction, move to the next
+    if(millis() - wiggleStart >= 250) {
+      leftWiggle = !leftWiggle;
+      wiggleStart = millis();
+    }
+
+    sorting = isBallPresent(); // sorting = turning;
+  }
 }
 
 /*
-                              TESTING:
+TESTING:
 
-  The following functions all relate to testing various parts of the robot.
+The following functions all relate to testing various parts of the robot.
 
-*/
+ */
 
 /*
-  Tests a wheel based on a target speed and the constants LEFT and RIGHT.
-*/
+   Tests a wheel based on a target speed and the constants LEFT and RIGHT.
+ */
 void testWheel(char wheel, int ts)
 {
   if(wheel == LEFT)
@@ -536,7 +598,7 @@ void testWheel(char wheel, int ts)
     writeToWheels(0, ts);
 }
 int readBackRight(){//gets data val from right infraread sensor IMH
- return analogRead(BACK_RIGHT_SENSOR);
+  return analogRead(BACK_RIGHT_SENSOR);
 }
 int readBackLeft(){//gets data val from left infraread sensor IMH
   return analogRead(BACK_LEFT_SENSOR);
@@ -563,63 +625,18 @@ void loop() {
     case 4:
       if(cornerState(LEFT)) state = 0;
     default:
-      for(int i = 50; i < 150; i += 5) {
-        sorter.write(i);
-        delay(2000);
-        printVar = i;
-      }
+      backToCornerState(SLOW_SPEED, 5);
       break;
   }
 
-	/*
-  // Color data to USB Serial
-  Serial.print("RED: ");
-  Serial.print(colorSensor.getRed());  
-  Serial.print("  GREEN: ");
-  Serial.print(colorSensor.getGreen());  
-  Serial.print("  BLUE: ");
-  Serial.print(colorSensor.getBlue());  
-  Serial.print("  WHITE: ");
-  Serial.print(colorSensor.getWhite());
-  Serial.println();
-  iterations++;
-	*/
-	/*
-  if(iterations == BLUETOOTH_LIMITER)
-  {
-    iterations = 0;
-    //Serial3.print("[ ");
-    for(int i = 0; i < 8; i++)
-    {
-      Serial3.print(sensors[i]);
-      Serial3.print(" ");
-    }
-
-    Serial3.println("]");
-    Serial3.print("State: ");
-    Serial3.println(state);
-    Serial3.print("Substate: ");
-    Serial3.println(printVar);
-    Serial3.println("Backup left: ");
-    Serial3.println(readBackLeft());
-    Serial3.println("----------------------------------------");
-    Serial3.print("Get Data = ");
-    Serial3.print(getBallData());
-    Serial3.print(" White val: ");
-    Serial3.println(colorSensor.getBlue());
-
-    int b = colorSensor.getBlue();
-    int w = colorSensor.getWhite();
-    int g = colorSensor.getGreen();
-    int r = colorSensor.getRed();
-    Serial3.print("Blue: ");
-    Serial3.println(b);
-    Serial3.print("Red: ");
-    Serial3.println(r);
-    Serial3.print("Green: ");
-    Serial3.println(g);
-    Serial3.print("White: ");
-    Serial3.println(w);
+  static int serialLimiter = millis();
+  if(millis() - serialLimiter >= 1000) {
+    Serial.print("Right Sensor: ");
+    Serial.println(readBackRight());
+    Serial.print("Left Sensor: ");
+    Serial.println(readBackLeft());
+    Serial.println("---------------------------");
+    serialLimiter = millis();
   }
-	*/
+
 }
--- Robot1_Code/constants.h
+++ Robot1_Code/constants.h
@@ -10,9 +10,9 @@ const int BALL_TRIGGER = 5000;
 const int SORT_TIME = 50;
 
 //speed constants
-const int SLOW_SPEED = 70;
-const int HALF_SPEED = 170;
-const int FULL_SPEED = 255;
+const int SLOW_SPEED = 50;
+const int HALF_SPEED = 100;
+const int FULL_SPEED = 150;
 
 //Joel constants
 const int TARGET_INDEX = 3;
@@ -29,12 +29,12 @@ const bool BACKWARDS        = false;
 
 const int R_DONT_DUMP = 100;
 const int L_DONT_DUMP = 32;
-const int R_DO_DUMP = 165;
+const int R_DO_DUMP = 35;
 const int L_DO_DUMP = 85;
 
 // arm constants
 const int L_ARM_DOWN = 90;
-const int R_ARM_DOWN = 153;
+const int R_ARM_DOWN = 159;
 const int L_ARM_UP = 170;
 const int R_ARM_UP = 80;
 const int L_ARM_HALF = 115;
@@ -42,11 +42,12 @@ const int R_ARM_HALF = 130;
 
 // Turn sequence
 const char TURN_SEQUENCE[] = {
-  LEFT, RIGHT, LEFT, LEFT, LEFT, LEFT, RIGHT, RIGHT, LEFT, RIGHT, RIGHT, RIGHT, LEFT, LEFT
+  RIGHT, LEFT, LEFT, RIGHT, LEFT, LEFT, LEFT, LEFT, RIGHT, RIGHT, LEFT, RIGHT, 
+  RIGHT, RIGHT, LEFT, LEFT
 };
 
 const char SECOND_TURN_SEQUENCE[] = {
-  RIGHT, RIGHT, RIGHT
+  RIGHT, RIGHT
 };
 
 
